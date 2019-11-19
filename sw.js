c189aa24240da641baa569ef390cbb6e6e6be26f
muratdu25 <muratcosgun25300@gmail.com>
2019-11-19 11:40:41 +0100

    new v

--- sw.js
+++ sw.js
@@ -19,6 +19,20 @@ self.addEventListener('install', (e) => {
       })
     );
   });
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
 
 var cacheName = 'scanette-v1';
 var appShellFiles = [
